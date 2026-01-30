45f40d3964d257e3ab4b407531ca8015039edfdd
Nitesh Sha <85791340+Fr0sTnUb@users.noreply.github.com>
2026-01-30 11:57:06 +0530

    Implement binary search algorithm for Leetcode Problem 704

---
+++ binary_search.cpp
@@ -0,0 +1,24 @@
+//Leetcode Problem 704: Binary Search
+
+class solution{
+    public: 
+    int binarySearch(vector<int>& arr, int target){
+        int left = 0;
+        int right = arr.size() - 1;
+
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+
+            if(arr[mid] == target){
+                return mid;
+            }
+            else if(arr[mid] < target){
+                left = mid + 1;
+            }
+            else{
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
