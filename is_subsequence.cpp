a9517af8166aec2ef370ac17896dd78555a18fd3
Nitesh Sha <85791340+Fr0sTnUb@users.noreply.github.com>
2026-02-04 12:28:22 +0530

    Implement isSubsequence function for Leetcode Problem 392: Is Subsequence

---
+++ is_subsequence.cpp
@@ -0,0 +1,18 @@
+//Leetcode Problem 392: Is Subsequence
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool isSubsequence(string s, string t) {
+    int sIndex = 0, tIndex = 0;
+
+    while (sIndex < s.length() && tIndex < t.length()) {
+        if (s[sIndex] == t[tIndex]) {
+            sIndex++;
+        }
+        tIndex++;
+    }
+
+    return sIndex == s.length();
+}
