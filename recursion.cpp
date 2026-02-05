e5f9a35c0f63fb006d610aadc11dbb9c718c9683
Nitesh Sha <85791340+Fr0sTnUb@users.noreply.github.com>
2026-02-05 15:59:59 +0530

    Refactor array printing function to reverse order using recursion

--- recursion.cpp
+++ recursion.cpp
@@ -1,21 +1,42 @@
 //Printing an array using recursion
 
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+
+// int n;
+// void Array(vector<int> arr, int i){
+//     if( i == n){
+//         return;
+//     }
+//     cout << arr[i] <<" ";
+//     Array(arr, i + 1);
+// }
+
+// int main(){
+//     vector<int> arr = {5, 4, 9, 3, 2, 1};
+//     n = arr.size();
+//     Array(arr, 0);
+//     return 0;
+// }
+
+//Printing an array in reverse order using recursion
+
 #include <iostream>
 #include <vector>
 using namespace std;
 
 int n;
-void Array(vector<int> arr, int i){
+void ReverseArray(vector<int> arr, int i){
     if( i == n){
         return;
     }
+    ReverseArray(arr, i + 1);
     cout << arr[i] <<" ";
-    Array(arr, i + 1);
 }
-
 int main(){
     vector<int> arr = {5, 4, 9, 3, 2, 1};
     n = arr.size();
-    Array(arr, 0);
+    ReverseArray(arr, 0);
     return 0;
 }
