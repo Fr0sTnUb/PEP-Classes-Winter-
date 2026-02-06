faa02eae002818dfe687eb7a0465f32c32c1182c
Nitesh Sha <85791340+Fr0sTnUb@users.noreply.github.com>
2026-02-06 13:23:55 +0530

    Refactor TargetElement function to include target search logic and remove ReverseArray implementation

--- recursion.cpp
+++ recursion.cpp
@@ -22,21 +22,47 @@
 
 //Printing an array in reverse order using recursion
 
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+
+// int n;
+// void ReverseArray(vector<int> arr, int i){
+//     if( i == n){
+//         return;
+//     }
+//     ReverseArray(arr, i + 1);
+//     cout << arr[i] <<" ";
+// }
+// int main(){
+//     vector<int> arr = {5, 4, 9, 3, 2, 1};
+//     n = arr.size();
+//     ReverseArray(arr, 0);
+//     return 0;
+// }
+
+//Printing a target element of an array using recursion
+
 #include <iostream>
 #include <vector>
 using namespace std;
 
 int n;
-void ReverseArray(vector<int> arr, int i){
+int TargetElement(vector<int> arr, int i, int target){
     if( i == n){
-        return;
+        return -1;
     }
-    ReverseArray(arr, i + 1);
-    cout << arr[i] <<" ";
+    if(arr[i] == target){
+        return i;
+    }
+    return TargetElement(arr, i + 1, target);
 }
+
 int main(){
     vector<int> arr = {5, 4, 9, 3, 2, 1};
     n = arr.size();
-    ReverseArray(arr, 0);
+    int target = 6;
+    int result = TargetElement(arr, 0, target);
+    cout << result;
     return 0;
 }
