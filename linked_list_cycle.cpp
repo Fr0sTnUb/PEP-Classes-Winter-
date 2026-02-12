cd20dbe74a7e1cbc29da36995a876c92dbfc1e35
Nitesh Sha <85791340+Fr0sTnUb@users.noreply.github.com>
2026-02-12 10:33:38 +0530

    Add implementation for Leetcode Problem 141: Linked List Cycle

---
+++ linked_list_cycle.cpp
@@ -0,0 +1,45 @@
+//Leetcode Problem 141: Linked List Cycle.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+  };
+ 
+ListNode* detectCycle(ListNode* A) {
+    if(A == NULL || A->next == NULL){
+        return NULL;
+    }
+    
+    ListNode* slow = A;
+    ListNode* fast = A;
+    
+    while(slow != NULL && fast != NULL){
+        slow = slow->next;
+        if(fast->next == NULL){
+            return NULL;
+        }
+        else{
+            fast = (fast->next)->next;
+        }
+        if(slow == fast){
+            break;
+        }
+    }
+    
+    if(slow == NULL || fast == NULL){
+        return NULL;
+    }
+    
+    ListNode* curr = A;
+    while(curr != slow){
+        curr = curr->next;
+        slow = slow->next;
+    }
+    
+    return curr;
+       
+}
