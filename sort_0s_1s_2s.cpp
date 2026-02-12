6069d7fd9b00965f21d399dcb2b925cef95be07a
Nitesh Sha <85791340+Fr0sTnUb@users.noreply.github.com>
2026-02-12 16:37:51 +0530

    Add alternative implementation for sorting a linked list of 0s, 1s, and 2s using dummy nodes

--- sort_0s_1s_2s.cpp
+++ sort_0s_1s_2s.cpp
@@ -36,3 +36,53 @@ class Solution {
         return head;
     }
 }
+
+//Different approach: Create three dummy nodes for 0s, 1s and 2s and then link them together.
+
+/*
+structure of the node of the list is as follows:
+struct Node {
+    int data;
+    Node* next;
+    Node(int x) : data(x), next(NULL) {}
+};
+
+class Solution {
+    public:
+    Node* segregate(Node *head){
+        if(head == nullptr || head->next == nullptr){
+            return head;
+        }
+        Node* zeroHead = new Node(0);
+        Node* oneHead = new Node(0);
+        Node* twoHead = new Node(0);
+        
+        Node* zeroTail = zeroHead;
+        Node* oneTail = oneHead;
+        Node* twoTail = twoHead;
+        
+        Node* curr = head;
+        while(curr != nullptr){
+            if(curr->data == 0){
+                zeroTail->next = curr;
+                zeroTail = zeroTail->next;
+            }
+            else if(curr->data == 1){
+                oneTail->next = curr;
+                oneTail = oneTail->next;
+            }
+            else{
+                twoTail->next = curr;
+                twoTail = twoTail->next;
+            }
+            curr = curr->next;
+        }
+        
+        zeroTail->next = (oneHead->next) ? (oneHead->next) : (twoHead->next);
+        oneTail->next = twoHead->next;
+        twoTail->next = nullptr;
+        
+        return zeroHead->next;        
+    }
+};
+*/
