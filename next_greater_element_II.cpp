ae592ec6c82e762263a45f1613445e9e9dd5a3f1
Nitesh Sha <85791340+Fr0sTnUb@users.noreply.github.com>
2026-02-18 15:08:49 +0530

    Add implementation for Next Greater Element II problem with two approaches

---
+++ next_greater_element_II.cpp
@@ -0,0 +1,44 @@
+//Leetcode Problem 503: Next Greater Element II
+//Time Complexity: O(n)
+
+class Solution{
+    public:
+    vector<int> nextGreaterElements(vector<int>& nums){
+        int n = nums.size();
+        vector<int> ans(n, -1);
+        stack<int> st;
+        for(int i=0; i<2*n; i++){
+            while(!st.empty() && nums[st.top()] < nums[i%n]){
+                ans[st.top()] = nums[i%n];
+                st.pop();
+            }
+            st.push(i%n);
+        }
+        return ans;
+    }
+};
+
+//Using Different Approach
+
+class Solution{
+    public:
+    vector<int> nextGreaterElements(vector<int>& nums){
+        int n = nums.size();
+        vector<int> ans(n, -1);
+        stack<int> st;
+        for(int i=0; i<n; i++){
+            while(!st.empty() && nums[st.top()] < nums[i]){
+                ans[st.top()] = nums[i];
+                st.pop();
+            }
+            st.push(i);
+        }
+        for(int i=0; i<n; i++){
+            while(!st.empty() && nums[st.top()] < nums[i]){
+                ans[st.top()] = nums[i];
+                st.pop();
+            }
+        }
+        return ans;
+    }
+};
