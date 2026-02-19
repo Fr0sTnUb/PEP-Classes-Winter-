873a1a13ee0f4e4baaa0eb29baf6e548774a4541
Nitesh Sha <85791340+Fr0sTnUb@users.noreply.github.com>
2026-02-19 19:13:12 +0530

    Add implementation for Queue Reversal problem

---
+++ queue.reversing.cpp
@@ -0,0 +1,12 @@
+//GFG Problem: Queue Reversal
+class Solution{
+    public:
+    void rev(queue<int> &q){
+        if(q.empty()) return;
+        int temp = q.front();
+        q.pop();
+        rev(q);
+        q.push(temp);
+    }
+    
+};
