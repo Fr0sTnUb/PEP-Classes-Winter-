7c9261b3633ee8c28751e5a878b1ac7ec979473f
Nitesh Sha <85791340+Fr0sTnUb@users.noreply.github.com>
2026-02-20 15:52:58 +0530

    Add implementation for reversing K elements of a queue

--- queue.reversing.cpp
+++ queue.reversing.cpp
@@ -9,3 +9,29 @@ class Solution{
         q.push(temp);
     }
 };
+
+
+//Reversing K elements of a queue
+class Solution{
+    public:
+    void rev(queue<int> &q, int k){
+        if(q.empty() || k <= 0) return;
+        stack<int> st;
+        
+        for(int i=0; i<k && !q.empty(); i++){
+            st.push(q.front());
+            q.pop();
+        }
+        
+        while(!st.empty()){
+            q.push(st.top());
+            st.pop();
+        }
+        
+        int size = q.size() - k;
+        for(int i=0; i<size; i++){
+            q.push(q.front());
+            q.pop();
+        }
+    }   
+};
