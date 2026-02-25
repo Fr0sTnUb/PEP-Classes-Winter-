bbc0e1e4b493ad567a5867d06e2419118d66d882
Nitesh Sha <85791340+Fr0sTnUb@users.noreply.github.com>
2026-02-25 15:25:28 +0530

    Add implementation for left side view of a binary tree

---
+++ binary_tree_left_side_view.cpp
@@ -0,0 +1,40 @@
+//GFG Problem: Binary Tree Left Side View
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+    public:
+    vector<int> leftSideView(TreeNode* root) {
+        queue<TreeNode*> q;
+        vector<vector<int>> lO;
+        if(!root) return {};
+        q.push(root);
+        while(!q.empty()){
+            vector<int> res;
+            int size = q.size();
+            for(int i = 0; i < size; i++){
+                TreeNode* node = q.front();
+                q.pop();
+                res.push_back(node->val);
+                if(node->left) q.push(node->left);
+                if(node->right) q.push(node->right);
+            }
+            lO.push_back(res);
+        }
+        vector<int> lsV;
+        for(int i = 0; i < lO.size(); i++){
+            lsV.push_back(lO[i][0]);
+        }
+        return lsV;
+    }
+};
