54683113a996a6991ae9987c48cfe4a32def222e
Nitesh Sha <85791340+Fr0sTnUb@users.noreply.github.com>
2026-03-08 00:31:46 +0530

    Refactor minFlips function to optimize flip counting for alternating binary string

--- POTD_LeetCode_#1.cpp
+++ POTD_LeetCode_#1.cpp
@@ -6,17 +6,25 @@ public:
         int n = s.size();
         string alt1, alt2;
         
-        for(int i = 0; i < n; i++) {
+        for(int i = 0; i < 2*n; i++) {
             alt1 += (i % 2 == 0) ? '0' : '1';
             alt2 += (i % 2 == 0) ? '1' : '0';
         }
+        s+=s;
         
         int flips1 = 0, flips2 = 0;
-        for(int i = 0; i < n; i++) {
+        int result = INT_MAX;
+        for(int i = 0; i < 2*n; i++) {
             if(s[i] != alt1[i]) flips1++;
             if(s[i] != alt2[i]) flips2++;
+
+            if(i >= n) {
+                if(s[i-n] != alt1[i-n]) flips1--;
+                if(s[i-n] != alt2[i-n]) flips2--;
+            }
+            if(i >= n-1) result = min(result, min(flips1, flips2));
         }
         
-        return min(flips1, flips2);
+        return result;
     }
 };
