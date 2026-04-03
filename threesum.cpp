c636aa626a6655b919e8f192238e38fc68da6caa
Nitesh Sha <85791340+Fr0sTnUb@users.noreply.github.com>
2026-04-03 21:45:52 +0530

    Add implementation for checking if a number is a palindrome

---
+++ threesum.cpp
@@ -0,0 +1,50 @@
+//Leetcode Question 15: 3Sum
+class Solution {
+public:   
+vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> result;
+        sort(nums.begin(), nums.end());
+        for(int i = 0; i < nums.size() - 2; i++){
+            if(i > 0 && nums[i] == nums[i - 1]) continue;
+            int left = i + 1;
+            int right = nums.size() - 1;
+            while(left < right){
+                int sum = nums[i] + nums[left] + nums[right];
+                if(sum == 0){
+                    result.push_back({nums[i], nums[left], nums[right]});
+                    while(left < right && nums[left] == nums[left + 1]) left++;
+                    while(left < right && nums[right] == nums[right - 1]) right--;
+                    left++;
+                    right--;
+                }
+                else if(sum < 0) left++;
+                else right--;
+            }
+        }
+        return result;
+    }
+};
+
+//Brute force approach using three nested loops
+/*
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> result;
+        sort(nums.begin(), nums.end());
+        for(int i = 0; i < nums.size() - 2; i++){
+            if(i > 0 && nums[i] == nums[i - 1]) continue;
+            for(int j = i + 1; j < nums.size() - 1; j++){
+                if(j > i + 1 && nums[j] == nums[j - 1]) continue;
+                for(int k = j + 1; k < nums.size(); k++){
+                    if(k > j + 1 && nums[k] == nums[k - 1]) continue;
+                    if(nums[i] + nums[j] + nums[k] == 0){
+                        result.push_back({nums[i], nums[j], nums[k]});
+                    }
+                }
+            }
+        }
+        return result;
+    }
+};
+*/
