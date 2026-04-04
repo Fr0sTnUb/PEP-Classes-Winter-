6a543b3ca172294644f5c699d137d3d0a2aa95a1
Nitesh Sha <85791340+Fr0sTnUb@users.noreply.github.com>
2026-04-04 23:13:09 +0530

    Add implementations for decoding slanted ciphertext and merging two sorted lists

---
+++ decode_the_slanted_cipher.cpp
@@ -0,0 +1,27 @@
+//Leetcode Problem 2075: Decode the Slanted Ciphertext
+
+class Solution {
+public:
+    string decodeCiphertext(string encodedText, int rows) {
+        int n = encodedText.size();
+        int cols = n / rows;
+
+        string ans;
+
+        for (int i = 0; i < cols; i++) {
+            int r = 0, c = i;
+
+            while (r < rows && c < cols) {
+                ans += encodedText[r * cols + c];
+                r++;
+                c++;
+            }
+        }
+
+        while (!ans.empty() && ans.back() == ' ') {
+            ans.pop_back();
+        }
+
+        return ans;
+    }
+};
